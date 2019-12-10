b52185fcd4242d5adf7a454c0967aef9768ad6a4
Daniel Khromov <danielkhromov@gmail.com>
2019-12-10 15:41:53 +0300

    First commit 10.12.19

---
+++ Projects/Практика10/Практика10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+namespace Практика9
+{
+    class Program
+    {
+        static int NumberOfLetters(string a)
+        {
+            int length = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (char.IsLetter(a[i])) length++;
+            }
+            return length;
+        }
+        static string BetweenBrackets(string a)
+        {
+            int leftBracket = a.IndexOf('(');
+            int rightBracket = a.IndexOf(')');
+            return a.Remove(leftBracket, rightBracket - leftBracket + 1);
+        }
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine("Количество букв: {0}", NumberOfLetters(a));
+            Console.WriteLine("Новая строка: {0}", BetweenBrackets(a));
+        }
+    }
+}
---
+++ Projects/Практика9/Практика9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Практика9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
