e287baad370c9fb929b2595dcb9f536223d56056
Daniel Khromov <danielkhromov@gmail.com>
2019-12-17 16:28:55 +0300

    Фоновая массивы 1.0

---
+++ Фоновая4вар/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Фоновая4вар
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = {1, 4, 3, 8, 3, 3, 9, 12};
+            int i = 0;
+            while (mas[i] % 2 != 0)
+                i++;
+
+            Console.WriteLine(mas[i]);
+
+
+
+            int n = 8;
+            int k = n - 1;
+            while (mas[k] % 3 == 0)
+                k--;
+
+            Console.WriteLine(mas[k]);
+        }
+    }
+}
---
+++ ФоноваяМассивы/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ФоноваяМассивы
+{
+    class Program
+
+    {
+        static int IndexOfFirstMax(int[] array)
+        {
+            int i = array[0], max = 0;
+            foreach (int element in array)
+                if (element > i) { max = element; break;} //TODO найти его индекс
+            return max;
+        }
+        static int IndexOfLastMin(int[] array)
+        {
+            int i = int.MaxValue, min = 0;
+            foreach (int element in array)
+                if (element < i) min = element; //TODO найти его индекс
+            return min;
+        }
+        static int[] Cin(int N)
+        {
+            int[] array = new int[N];
+            for(int i = 0; i < N; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Cin(N);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
