d106cc5f041b2e56e288e8a20d4aa2fb465db7d8
Daniel Khromov <danielkhromov@gmail.com>
2019-12-17 09:52:35 +0300

    Фоновая 3.1

--- Projects/Практика10/Практика10/Program.cs
+++ Projects/Практика10/Практика10/Program.cs
@@ -6,24 +6,26 @@ namespace Практика9
     {
         static int NumberOfLetters(string a)
         {
-            int length = 0;
+            int l = 0;
             for (int i = 0; i < a.Length; i++)
             {
-                if (char.IsLetter(a[i])) length++;
+                if (char.IsLetter(a[i])) l++;
             }
-            return length;
+            return l;
         }
-        static string BetweenBrackets(string a)
+        static void BetweenBrackets(ref string a)
         {
             int leftBracket = a.IndexOf('(');
             int rightBracket = a.IndexOf(')');
-            return a.Remove(leftBracket, rightBracket - leftBracket + 1);
+            a = a.Remove(leftBracket, rightBracket - leftBracket + 1);
         }
         static void Main(string[] args)
         {
             string a = Console.ReadLine();
             Console.WriteLine("Количество букв: {0}", NumberOfLetters(a));
-            Console.WriteLine("Новая строка: {0}", BetweenBrackets(a));
+            BetweenBrackets(ref a);
+            Console.WriteLine(a);
+
         }
     }
 }
